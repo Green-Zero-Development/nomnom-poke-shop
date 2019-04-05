59159e79ae11e1435c84a1b8112894f595fed808
Animated-Eagle <sheddinghorribly@gmail.com>
2019-04-05 14:00:29 -0400

    finalize home page

--- themes/site-theme/src/js/main.js
+++ themes/site-theme/src/js/main.js
@@ -17,26 +17,38 @@ $(function() {
 });
 
 $(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+$(document).ready(function(){
   $('.food-slider').slick({
     arrows: true,
     infinite: true,
     dots: true,
-    fade: true,
     dotsClass: 'flex w-full mx-auto food-slider-dots',
-    prevArrow: '<i class="absolute text-4xl text-white z-50 pt-32 mt-32 pl-5 fas fa-chevron-left slick-arrow"></i>',
-    nextArrow: '<i class="absolute text-4xl text-white z-50 pin-r pin-t pt-32 mt-32 pr-5 fas fa-chevron-right slick-arrow"></i>',
+    prevArrow: '<i class="absolute text-6xl text-white z-50 pt-32 mt-32 pl-5 opacity-50 hover:opacity-100 hover:cursor-pointer fas fa-chevron-left slick-arrow"></i>',
+    nextArrow: '<i class="absolute text-6xl text-white z-50 pin-r pin-t pt-32 mt-32 pr-5 opacity-50 hover:opacity-100 hover:cursor-pointer fas fa-chevron-right slick-arrow"></i>',
     speed: 500,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          fade: false,
-          arrows: false,
-          infinite: true,
-          dots: true,
-          speed: 500,
-        }
-      }
-    ]
   });
 });
